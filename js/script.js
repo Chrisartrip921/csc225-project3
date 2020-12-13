a8a00fc11dadb16688699518c953f672d88e1a72
chris artrip <christopherartrip@gmail.com>
2020-12-13 01:03:11 -0500

    second commit. Added cards to display console info upon click function.

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($){
             
                 <div data-id="${id}" class="media my-5 hover-background-gray cursor-pointer">
                 
-                    <img src="${image}" height="128px" width="258px" alt="Photo of ${name}">
+                    <img src="${image}" height="128px" width="258px" alt="image of ${name}">
                     
                     <div class="media-body">
                         <h5>${name}</h5>
@@ -31,8 +31,30 @@ jQuery(document).ready(function($){
     });
     
     jQuery('#consoles').on('click','.media', function(){
-        alert('I have been clicked');
-
+        const id = $(this).attr('data-id');
+        
+        const cnsleUrl = `http://csc225.mockable.io/consoles/${id}`;
+        axios.get(cnsleUrl).then(function(load){
+            const { id, name, image } = load.data;
+            $('#console').html(
+                `
+                
+                <div class="card text-center centerCard" style="width: 18rem;">
+                    <img src="${image}" class="card-img-top" alt="image of ${name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${name}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            
+                
+                
+                `
+            );
+        }).catch(function(error){
+            alert('ERROR!');
+        });
     });
     
 
